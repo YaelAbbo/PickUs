4adcc41e86afe15f0ff1d3b3dfc02ef6430a2fc1
Natan Sinai <natanbriman@gmail.com>
2026-03-03 19:14:17 +0200

    remove log

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,30 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const x = {
-          type: 'postgres',
-          host: configService.get('POSTGRES_HOST'),
-          port: configService.get<number>('POSTGRES_PORT'),
-          username: configService.get('POSTGRES_USER'),
-          password: configService.get('POSTGRES_PASSWORD'),
-          database: configService.get('POSTGRES_DB'),
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          logging: configService.get('NODE_ENV') === 'development',
-          synchronize: false,
-          migrationsRun: true,
-        } as TypeOrmModuleOptions;
-
-        console.log(x);
-
-        return x;
-      },
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        logging: configService.get('NODE_ENV') === 'development',
+        synchronize: false,
+        migrationsRun: true,
+      }),
     }),
   ],
 })
