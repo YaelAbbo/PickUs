87814d5de23c5354d566ec567f81922513169a25
Natan Sinai <natanbriman@gmail.com>
2026-03-03 22:58:01 +0200

    fix metro

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,10 +1,7 @@
-import { getDefaultConfig } from 'expo/metro-config';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+/* eslint-disable */
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-export default config;
+module.exports = config;
