3772f186d76177df660c01cae98552027cb70a48
Natan Sinai <natanbriman@gmail.com>
2026-03-03 19:42:27 +0200

    fix migrations

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
+        migrations: [join(__dirname, './migrations/*.{ts,js}')],
         logging: configService.get('NODE_ENV') === 'development',
         synchronize: false,
         migrationsRun: true,
