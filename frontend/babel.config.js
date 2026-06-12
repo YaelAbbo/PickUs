0101df44faeac018876d10b529fbf2b7191f1994
Natan Sinai <natanbriman@gmail.com>
2026-06-12 12:18:02 +0300

    fix plugins

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,5 +3,14 @@ export default function (api) {
 
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      // 1. MUST run TypeScript transform fir st to handle 'declare' fields in Expo modules
+      ['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
+
+      // 2. Transpile the private properties so Hermes doesn't crash on #x or #y
+      '@babel/plugin-transform-class-properties',
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-transform-private-property-in-object',
+    ],
   };
 }
