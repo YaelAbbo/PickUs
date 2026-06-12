d16f9f16f14789721951d7775b25307c5dd164cc
Natan Sinai <natanbriman@gmail.com>
2026-06-12 11:47:53 +0300

    fix: package install on monorepo

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,5 +3,10 @@ export default function (api) {
 
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-transform-class-properties',
+      '@babel/plugin-transform-private-property-in-object',
+    ],
   };
 }
