45b73c530d9bfda3aebd80d7cd931302b68caae4
YaelAbbo <yaelabbo@gmail.com>
2026-06-13 13:54:34 +0300

    try to fix

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -3,10 +3,6 @@ module.exports = function (api) {
 
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    ],
+    plugins: ['@babel/plugin-transform-private-methods', '@babel/plugin-transform-private-property-in-object'],
   };
 };
