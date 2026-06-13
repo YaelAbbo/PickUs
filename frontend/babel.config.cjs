6038de859c520227e38ad799c371b3d7e7524d2a
YaelAbbo <yaelabbo@gmail.com>
2026-06-13 13:38:07 +0300

    try to fix

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -3,6 +3,5 @@ module.exports = function (api) {
 
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['@babel/plugin-transform-private-methods', '@babel/plugin-transform-class-properties'],
   };
 };
