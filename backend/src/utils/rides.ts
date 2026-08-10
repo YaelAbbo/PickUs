67b3cbe1dc08958d5356b48bdace9f91581a004b
Natan Sinai <natanbriman@gmail.com>
2026-08-10 17:55:22 +0300

    fix available rides query and live ride passenger events

--- backend/src/utils/rides.ts
+++ backend/src/utils/rides.ts
@@ -1,7 +1,13 @@
-import type { Ride } from '@/database/entities';
+import type { Ride, User } from '@/database/entities';
 
-export const filterAvailableRides = (rides: Ride[]) =>
+export const filterAvailableRides = (rides: Ride[], userId: User['id']) =>
   rides.filter((ride) => {
+    const isUserPassenger = ride.passengers.some(
+      (ridePassenger) => ridePassenger.userId === userId,
+    );
+
+    if (isUserPassenger) return false;
+
     const passengerCount = ride.passengers?.length || 0;
     const availableSeats = ride.maxSeatsAmount - passengerCount;
     return availableSeats > 0;
